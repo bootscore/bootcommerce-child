6dc966193b88a18a0182b661f6af202336b527ae
Upul Abayagunawardhana <uirosha@gmail.com>
2022-04-11 05:13:33 +0530

    Fixed site footer. Made Prices bolder

--- footer.php
+++ footer.php
@@ -8,13 +8,15 @@
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package Bootscore
+ *
+ * @version 5.2.0.0
  */
 
 ?>
 
-<footer>
+<footer class="site-footer" style="background-image: url(<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/img/footer-bg.png);">
 
-  <div class="bootscore-footer bg-light pt-5 pb-3">
+  <div class="bootscore-footer pt-5 pb-3">
     <div class="container">
 
       <!-- Top Footer Widget -->
@@ -90,9 +92,8 @@
 
 </footer>
 
-<div class="top-button position-fixed zi-1020">
-  <a href="#to-top" class="btn btn-primary shadow"><i class="fas fa-chevron-up"></i><span class="visually-hidden-focusable">To top</span></a>
-</div>
+<!-- To top button -->
+<a href="#" class="btn btn-primary shadow top-button position-fixed zi-1020"><i class="fa-solid fa-chevron-up"></i><span class="visually-hidden-focusable">To top</span></a>
 
 </div><!-- #page -->
 
